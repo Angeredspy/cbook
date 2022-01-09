9c0d3c48948af72d150799d281e04dddcd0f62bb
Angeredspy <christo.graham1@gmail.com>
2022-01-08 22:18:37 -0500

    adds custom plugins for onResolve and onLoad event listeners

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
-import { useState, useEffect, useRef} from 'react';
-import * as esbuild from 'esbuild-wasm'; 
+import { useState, useEffect, useRef } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
   const ref = useRef<any>();
@@ -8,31 +9,43 @@ const App = () => {
   const [code, setCode] = useState('');
 
   const startService = async () => {
-      ref.current = await esbuild.startService({
-        worker: true, 
-        wasmURL: '/esbuild.wasm'
-      }); 
-      
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
   };
 
-  useEffect( () => {
-      startService(); 
-  }, [])
+  useEffect(() => {
+    startService();
+  }, []);
 
   const onClick = async () => {
     if (!ref.current) {
-        return;
-    } 
-    const result = await ref.current.transform(input, {
+      return;
+    }
+    /* const result = await ref.current.transform(input, {
         loader: 'jsx', 
         target: 'es2015',
-    }); // This does transpiling.
-    setCode(result.code);
+    }); // This does transpiling. 
+  */
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    }); //This does bundling.
+    console.log(result);
+
+    setCode(result.outputFiles[0].text);
   };
 
   return (
     <div>
-      <textarea value={input} onChange={ e => setInput(e.target.value)}></textarea>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
